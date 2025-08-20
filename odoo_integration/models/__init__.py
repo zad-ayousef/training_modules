0d20ad3ab39bf36f6648b6da69783d4dee7bc532
zad-ayousef <ayousef@zadsolutions.com>
2025-08-20 11:49:49 +0300

    Delete odoo_integration directory

--- odoo_integration/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import res_config_setting_extend
-from . import project_project_extend
-from . import project_task_extend
-from . import project_task_type_extend
-from . import mail_message_extend
-
